d521d1c08703084a72cdb499c2ac495d00c324eb
FlowerPower2 <bibifeldman@gmail.com>
2020-11-02 01:04:35 +0200

    better carousel

--- Grenade/src/components/views/Games.jsx
+++ Grenade/src/components/views/Games.jsx
@@ -1,29 +1,79 @@
 import React from "react";
-import Carousel from "../Carousel/Carousel";
 // import PropTypes from 'prop-types'
 
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
 function Games() {
+    var settings = {
+        className: "center",
+        centerMode: true,
+        centerPadding: "60px",
+        dots: true,
+        infinite: true,
+        autoplay: true,
+        speed: 2000,
+        autoplaySpeed: 2000,
+        cssEase: "linear",
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        initialSlide: 0,
+        pauseOnHover: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true,
+                },
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    initialSlide: 2,
+                },
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+    };
     return (
-        <div className="">
-            <Carousel
-                title="new games"
-                games={[
-                    "a",
-                    "b",
-                    "c",
-                    "d",
-                    "b",
-                    "c",
-                    "d",
-                    "b",
-                    "c",
-                    "d",
-                    "b",
-                    "c",
-                    "d",
-                ]}
-            />
-        </div>
+        <Slider {...settings}>
+            <div>
+                <h3>1</h3>
+            </div>
+            <div>
+                <h3>2</h3>
+            </div>
+            <div>
+                <h3>3</h3>
+            </div>
+            <div>
+                <h3>4</h3>
+            </div>
+            <div>
+                <h3>5</h3>
+            </div>
+            <div>
+                <h3>6</h3>
+            </div>
+            <div>
+                <h3>7</h3>
+            </div>
+            <div>
+                <h3>8</h3>
+            </div>
+        </Slider>
     );
 }
 
